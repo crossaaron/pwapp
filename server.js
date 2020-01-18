165ff40ac76f49884f704be287e9f8a759bc43a2
crossaaron <atcbcrossx@gmail.com>
2020-01-17 20:00:12 -0500

    .env setup

--- server.js
+++ server.js
@@ -17,7 +17,7 @@
  * limitations under the License
  */
 'use strict';
-
+require("dotenv").config();
 const express = require('express');
 const fetch = require('node-fetch');
 const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
@@ -28,7 +28,6 @@ const FORECAST_DELAY = 0;
 // CODELAB: If running locally, set your Dark Sky API key here
 const API_KEY = process.env.DARKSKY_API_KEY;
 const BASE_URL = `https://api.darksky.net/forecast`;
-
 // Fake forecast data used if we can't reach the Dark Sky API
 const fakeForecast = {
   fakeData: true,
@@ -121,6 +120,8 @@ const fakeForecast = {
  * @return {Object} forecast object.
  */
 function generateFakeForecast(location) {
+  console.log(API_KEY);
+
   location = location || '40.7720232,-73.9732319';
   const commaAt = location.indexOf(',');
 
